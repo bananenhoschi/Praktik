b7363159e3b3ac517fc2f68cd8d28f9827a372b7
Carlo Eggenschwiler <carlo.eggenschwiler@postfinance.ch>
2021-07-06 23:40:12 +0200

    hard constraint 1 and 3

--- praktik-backend/src/main/kotlin/ch/postfinance/ccyp/praktik/constraints/AllocationConstraintProvider.kt
+++ praktik-backend/src/main/kotlin/ch/postfinance/ccyp/praktik/constraints/AllocationConstraintProvider.kt
@@ -1,6 +1,5 @@
 package ch.postfinance.ccyp.praktik.constraints
 
-import ch.postfinance.ccyp.praktik.domain.Application
 import ch.postfinance.ccyp.praktik.domain.Employee
 import ch.postfinance.ccyp.praktik.domain.Job
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore
@@ -13,28 +12,23 @@ class AllocationConstraintProvider : ConstraintProvider {
 
     override fun defineConstraints(constraintFactory: ConstraintFactory): Array<Constraint> {
         return arrayOf(
-            jobConstraint(constraintFactory),
-            profileConstraint(constraintFactory),
-            applicationConstraint(constraintFactory)
+                jobConstraint(constraintFactory),
+                profileConstraint(constraintFactory)
         )
     }
 
-    private fun applicationConstraint(constraintFactory: ConstraintFactory): Constraint {
-        return constraintFactory.from(Application::class.java)
-            .join(Employee::class.java)
-            .join(Job::class.java)
-            .penalize("application constraint", HardSoftScore.ONE_HARD)
-    }
-
+    // An employee and a job must have the same profile
     private fun profileConstraint(constraintFactory: ConstraintFactory): Constraint {
         return constraintFactory.from(Employee::class.java)
-            .join(Employee::class.java, Joiners.equal(Employee::profile))
-            .penalize("profile constraint", HardSoftScore.ONE_HARD)
+                .join(Job::class.java, Joiners.equal(Employee::profile, Job::profile))
+                .penalize("profile constraint", HardSoftScore.ONE_HARD)
     }
 
+    // An employee can only be assigned to one job
     private fun jobConstraint(constraintFactory: ConstraintFactory): Constraint {
-        return constraintFactory.fromUniquePair(Employee::class.java, Joiners.equal(Employee::job))
-            .penalize("job constraint", HardSoftScore.ONE_HARD)
+        return constraintFactory.from(Employee::class.java)
+                .join(Employee::class.java, Joiners.equal(Employee::job))
+                .penalize("job constraint", HardSoftScore.ONE_HARD);
     }
 
 
